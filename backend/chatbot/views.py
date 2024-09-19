6e014fe4df1ef90c3c6fb5ef4a4b26494a27f8ff
samuelawud123 <samuelawud@icloud.com>
2024-09-18 19:45:20 -0700

    Set up Django backend and React frontend

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def get_data(request):
+    data = [
+        {'id': 1, 'name': 'Item 1'},
+        {'id': 2, 'name': 'Item 2'},
+    ]
+    return Response(data)
+
