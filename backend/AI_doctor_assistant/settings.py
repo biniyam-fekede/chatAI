a4f513ee8d82ecec347d1152bb49fca3f42b64d2
samuelawud123 <samuelawud@icloud.com>
2024-10-01 08:24:47 -0700

    Replaced SQLite with PostgresSQL

--- backend/AI_doctor_assistant/settings.py
+++ backend/AI_doctor_assistant/settings.py
@@ -59,10 +59,15 @@ WSGI_APPLICATION = 'AI_doctor_assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
